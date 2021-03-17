9ee194d48b516575017320addd27ffc5899c983d
gaoht <ght5935@163.com>
2021-03-17 16:58:29 +0800

    手写柯里化

--- javascript/手写系列/5.手写一个柯里化.js
+++ javascript/手写系列/5.手写一个柯里化.js
@@ -1,20 +1,69 @@
 
 /*
-柯里化是什么：是指这样一个函数，它接收函数 A，并且能返回一个新的函数，这个新的函数能够处理函数 A 的剩余参数
+柯里化通常也称部分求值，其含义是给函数分步传递参数，每次传递参数后部分应用参数，
+并返回一个更具体的函数接受剩下的参数，这中间可嵌套多层这样的接受部分参数函数，
+直至返回最后结果
 */
 
-function createCurry(func, args) {
-    var argity = func.length;
-    var args = args || [];
+function add() {
+    // 用来缓存所有的arguments值  
+    let args = [...arguments]
+    // 新建fn函数实现柯里化 
+    let fn = function () {
+        // 如果参数为空，那么递归停止，返回执行结果
+        if (arguments.length === 0) {
+            return args.reduce((a, b) => a + b)
+        } else {
+            // 否则将参数保存到args里面，返回fn方法
+            args.push(...arguments)
+            return fn
+        }
+    }
+    return fn
+}
+console.log(add(1, 2)(3)(4)(5, 6, 7)(9)())
+
+/*
+ * 因为currying函数里面使用arguments，所以currying不能使用箭头函数，
+ *  箭头函数内部的arguments的用法与箭头函数内部的this差不多，它取的是上一级函数的arguments值。
+ * 如果想用箭头函数，currying函数可以这样改动
+ */
+
+function add1() {
+    // 用来缓存所有的arguments值  
+    let args = [...arguments]
+    // 新建fn函数实现柯里化 
+    let fn = (...rest) => {
+        // 如果参数为空，那么递归停止，返回执行结果
+        if (rest.length === 0) {
+            return args.reduce((a, b) => a + b)
+        } else {
+            // 否则将参数保存到args里面，返回fn方法
+            args.push(...rest)
+            return fn
+        }
+    }
+    return fn
+}
+console.log(add1(1, 2)(3)(4)(5, 6, 7)(9)())
+
+/**
+ * 我们返回的fn函数还可以使用callee来实现，原理相同，但是严格模式下不能使用：
+ */
+function add2() {
+    // 用来缓存所有的arguments值  
+    let args = [...arguments]
 
     return function () {
-        var _args = [].slice.apply(arguments);
-        args.push(..._args);
-
-        if (args.length < argity) {
-            return createCurry.call(this, func, args);
+        // 如果参数为空，那么递归停止，返回执行结果
+        if (arguments.length === 0) {
+            return args.reduce((a, b) => a + b)
+        } else {
+            // 否则将参数保存到args里面，返回方法
+            args.push(...arguments)
+            return arguments.callee
         }
-
-        return func.apply(this, args);
     }
+    return fn
 }
+console.log(add2(1, 2)(3)(4)(5, 6, 7)(9)())
